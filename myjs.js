5e161a5235f5f7678647a9a40358df9c372a1a77
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-10-02 09:33:00 -0500

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -54,3 +54,18 @@
   }
 
   document.addEventListener('DOMContentLoaded', () => enhanceElides());
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
