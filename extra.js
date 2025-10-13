4fb7cc1b55d6bcb53242667e9ae055bf08eb460e
Matthew Mac Kelly <171302743+ProfMacKelly@users.noreply.github.com>
2025-10-13 13:52:51 -0500

    Create extra.js

---
+++ extra.js
@@ -0,0 +1,22 @@
+function toggleText(element) {
+    let hiddenText = element.previousElementSibling;
+    
+    if (hiddenText.style.display === "none" || hiddenText.style.display === "") {
+        hiddenText.style.display = "inline";
+        element.textContent = " ⬅"; // Change ellipsis to collapse symbol
+    } else {
+        hiddenText.style.display = "none";
+        element.textContent = ". . ."; // Restore ellipsis
+    }
+}
+function toggleAnswer(button) {
+    let answer = button.previousElementSibling;
+
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+        button.textContent = "Hide Answer";
+    } else {
+        answer.style.display = "none";
+        button.textContent = "Answer";
+    }
+}
